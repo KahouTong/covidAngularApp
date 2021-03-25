f1cc541965104a732006385f6c2d3200713ef902
KahouTong <elric.vlcy@gmail.com>
2021-03-25 12:00:54 +0800

    updates

---
+++ ignoreFiles.bat
@@ -0,0 +1,7 @@
+Title Ignore Folder 
+
+git add .scannerwork .gitignore -f
+git add C:\Users\ASUS\eclipse-workspace\Week 9 java EE\covidAngularApp\covid-app\node_modules .gitignore -f
+git add .vscode .gitignore -f
+
+pause
